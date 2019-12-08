837d41094d6b93ec521b12d2a1478353ee7ef2c1
Vincent <v1996.vincent@gmail.com>
2019-12-08 17:29:11 +0100

    add exo5

--- assets/Js/script_ex5.js
+++ assets/Js/script_ex5.js
@@ -1,58 +1,74 @@
 let b =2;
 let a;
-step();
-function step(){
-    a = b++;
-    alert (a);
-    console.log(a)
-    reroll()
-    console.log(a)
-    step1();
-}
-function step1(){
-    a = ++b;
-    alert (a);
-    console.log(a)
-    reroll()
-    console.log(a)
-    step2();
-}
-function step2(){
-    a =b--;
-    alert (a);
-    console.log(a)
-    reroll()
-    console.log(a)
-    step3()
-}
-function step3(){
-    a =--b;
-    alert (a);
-    console.log(a)
-    reroll()
-    console.log(a)
-    step4()
-
-}
-function step4(){
-    a+=b++;5
-    alert (a)
-}
-function step5(){
-
-}
-function step6(){
-
-}
-function step7(){
-
-}
-function step8(){
-
-}
-function reroll(){
-    a= 0;
-}
+document.getElementById("run").addEventListener('click',()=>{
+    alert ("Début des opération")
+    step()
+    function step(){
+        a = b++;
+        alert ("a=b++ => "+a);
+        reroll();
+        step1();
+    }
+    function step1(){
+        a = ++b;
+        alert ("a=++b => "+a);
+        reroll();
+        step2();
+    }
+    function step2(){
+        a =b--;
+        alert ("a=b-- => "+a);
+        reroll();
+        step3();
+    }
+    function step3(){
+        a =--b;
+        alert ("a=--b => "+a);
+        reroll();
+        step4();
+    
+    }
+    function step4(){
+        a+=b++;
+        alert ("a+=b++ => "+a);
+        reroll();
+        step5();
+    }
+    function step5(){
+        a= 1;
+        a+=++b;
+        alert ("a=1;\na+=++b => "+a);
+        reroll();
+        step6();
+    
+    }
+    function step6(){
+        a=1;
+        a-=b++;
+        alert ("a=1:\na-=b++ =>"+a);
+        reroll();
+        step7();
+    }
+    function step7(){
+        a=1;
+        a-=++b;
+        alert ("a=1;\na-=++b => "+a);
+        reroll();
+        step8();
+    }
+    function step8(){
+        a=1;
+        a+=b--;
+        alert ("a=1;\na+=b-- => "+a);
+        reroll();
+        alert ("Fin des opération")
+    
+    }
+    function reroll(){
+        ("reset a")
+        a= 0;
+    }
+})
 //todo les operation a realiser 
 /*
 a=b++;1
